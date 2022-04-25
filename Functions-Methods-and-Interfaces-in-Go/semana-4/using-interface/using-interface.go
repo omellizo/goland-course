4c5edde7b51242cb195d0cba0e10131e842a2111
Oscar MA <c.omellizo@sic.gov.co>
2022-04-25 10:09:15 -0500

    Adicion de metodo para assertion con switch

--- Functions-Methods-and-Interfaces-in-Go/semana-4/using-interface/using-interface.go
+++ Functions-Methods-and-Interfaces-in-Go/semana-4/using-interface/using-interface.go
@@ -31,14 +31,14 @@ func (t Rectangle) Perimeter() float64 {
 	return 0
 }
 
-func FitInYard (s Shape2D) bool {
+func FitInYard(s Shape2D) bool {
 	if s.Area() > 100 && s.Perimeter() > 100 {
 		return true
 	}
 	return false
 }
 
-func DrawShape (s Shape2D) {
+func DrawShape(s Shape2D) {
 	rect, ok := s.(Rectangle)
 	if ok {
 		fmt.Println("Is a Rectangle", rect)
@@ -49,11 +49,22 @@ func DrawShape (s Shape2D) {
 	}
 }
 
+func DrawShapeSwitch(s Shape2D) {
+	switch sh := s.(type) {
+	case Rectangle:
+		fmt.Println("Is a Rectangle", sh)
+	case Triangle:
+		fmt.Println("Is a Triangle", sh)
+	}
+}
+
 func main() {
 	triangle := Triangle{"tri1"}
 	DrawShape(triangle)
+	DrawShapeSwitch(triangle)
 
 	rectangle := Rectangle{"rect1"}
 	DrawShape(rectangle)
+	DrawShapeSwitch(rectangle)
 
 }
